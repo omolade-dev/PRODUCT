0b4943070c8a029a884ff5490e484b2cb122b2cc
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-02 13:17:48 +0100

    Add initial project setup with React, Vite, and basic components
    - Created main entry point (main.jsx) for React application.
    - Added Vite configuration file (vite.config.js) for project setup.
    - Implemented a basic Navbar component with category filters.
    - Included SVG assets for React and Vite logos.
    - Added global CSS file (index.css) for styling.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <div classname="logo">
+        <h1>Product Store!</h1>
+
+        <p>Browse our collection of products</p>
+
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <div>
+      <h2>FILTER BY CATEGORY </h2>
+
+      <div className="btn">
+        <button> ALL </button>
+        <button> ALL </button>
+        <button> ALL </button>
+        <button> ALL </button>
+        <button> ALL </button>
+        <button> ALL </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
