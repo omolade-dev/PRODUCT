302042f7b0446f42e5865f9bd26acee332134168
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-04 20:19:57 +0100

    Refactor App component to accept props and improve structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import Navbar from "./components/Navbar";
 
-function App() {
+function App(props) {
   return (
     <div>
       <div classname="logo">
         <h1>Product Store!</h1>
 
         <p>Browse our collection of products</p>
+      </div>
+      <Navbar />
 
-        <Navbar />
+      <p>Showing 10 products</p>
+      <div className="card">
+        <div className="top">{props.img}</div>
 
-        <p>Showing 10 products</p>
+        <div className="bottom">
+          <div>
+            <h3>{props.name}</h3>
+            <p>{props.category}</p>
+          </div>
+
+          <div>
+            <h3>{props.price}</h3>
+            <p>In Stock</p>
+          </div>
+        </div>
       </div>
     </div>
   );
